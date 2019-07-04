e69d50c96d05e09719419ec676901b8698f91e3a
lisabg <51778565+lisabg@users.noreply.github.com>
2019-07-04 10:36:17 +0200

    added styling and create user button

--- src/components/login.js
+++ src/components/login.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import './general.css';
-import { Button, InputGroup, Input, InputGroupAddon, InputGroupText, UncontrolledAlert} from 'reactstrap';
+import {
+    Button,
+    InputGroup,
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    UncontrolledAlert,
+    Form,
+    Row,
+    Col, FormGroup, Label
+} from 'reactstrap';
 import image from '../uploads/vaca.png';
 import axios from 'axios';
 
 export default class Login extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        exists: false,
-        email: "",
-        errorMsg: null,
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            exists: false,
+            email: "",
+            errorMsg: null,
+        }
     }
-  
+
     onEmailChange = event => {
-      this.setState({email: event.target.value});
+        this.setState({email: event.target.value});
     };
 
     goToUserPage = () => {
-      this.props.history.push("/user");
+        this.props.history.push("/user");
     };
 
     onErrorMsgChange = (errorMsg) => {
@@ -31,72 +41,91 @@ export default class Login extends React.Component {
     };
 
 
-    fetchUser = async() => {
+    fetchUser = async () => {
         this.onErrorMsgChange(null);
-      const user = {
-        email: this.checkMail(this.state.email)
-      };
-      const response = await axios.post('https://sysco-feri.herokuapp.com/api/login', user)
-          .catch(error => {
-              this.setState({email: ""});
-              this.onErrorMsgChange("something went wrong! ");
-          });
-        
-      console.log(response);
-      if(response !== null && response !== undefined) {
-          if (response.status === 200) {
-              this.props.setLoggedIn(response.data);
-              if (this.props.loggedIn) {
-                  this.goToUserPage();
-              }
-          }else {
-              // to empty the input field
-              this.setState({email: ""});
-              this.onErrorMsgChange("something went wrong!");
-          }
-      } else{
-          this.setState({email: ""});
-          this.onErrorMsgChange("something went wrong!");
-      }
+        const user = {
+            email: this.checkMail(this.state.email)
+        };
+        const response = await axios.post('https://sysco-feri.herokuapp.com/api/login', user)
+            .catch(error => {
+                this.setState({email: ""});
+                this.onErrorMsgChange("Something went wrong! ");
+            });
+
+        console.log(response);
+        if (response !== null && response !== undefined) {
+            if (response.status === 200) {
+                this.props.setLoggedIn(response.data);
+                if (this.props.loggedIn) {
+                    this.goToUserPage();
+                }
+            } else {
+                // to empty the input field
+                this.setState({email: ""});
+                this.onErrorMsgChange("Something went wrong!");
+            }
+        } else {
+            this.setState({email: ""});
+            this.onErrorMsgChange("Please type in your username again or click 'Sign up'");
+        }
     };
 
     checkMail = (mail) => {
-        if(!mail.includes("@sysco.no")){
+        if (!mail.includes("@sysco.no")) {
             return mail + "@sysco.no"
-        }else{
+        } else {
             return mail
         }
     };
 
     renderErrorMsg = () => {
-        if(this.state.errorMsg !== null && this.state.errorMsg.length > 0){
-           return (<UncontrolledAlert color="primary">
+        if (this.state.errorMsg !== null && this.state.errorMsg.length > 0) {
+            return (<UncontrolledAlert color="primary">
                 {this.state.errorMsg}
             </UncontrolledAlert>);
-        }else{
+        } else {
             return <div/>
         }
     };
 
-    render(){
-      return (
-          <div>
-              {this.renderErrorMsg()}
+    render() {
+        return (
+            <div>
+                {this.renderErrorMsg()}
                 <div className="login">
                     <div className="contentLogin">
-                    <InputGroup id="inputGroup" size="lg">
-                      <Input placeholder="username" name="name" value={this.state.email} onChange={e => this.onEmailChange(e)}/>
-                        <InputGroupAddon addonType="append">
-                            <InputGroupText>@sysco.no</InputGroupText>
-                          </InputGroupAddon>
-                    </InputGroup>
-                    <Button id="signIn" value="approved" onClick= {() => this.fetchUser()}>Sign in</Button>
+                        <InputGroup id="inputGroup" size="lg">
+                            <Input placeholder="username" name="name" value={this.state.email}
+                                   onChange={e => this.onEmailChange(e)}/>
+                            <InputGroupAddon addonType="append">
+                                <InputGroupText>@sysco.no</InputGroupText>
+                            </InputGroupAddon>
+                        </InputGroup>
+                        <React.Fragment>
+                            <Form className="buttons">
+                                <Row form>
+                                    <Col sm={3}/>
+                                    <Col sm={3}>
+                                        <FormGroup>
+                                            <Button id="signInPageButtons" value="approved" onClick={() => this.fetchUser()}>Sign in</Button>
+                                        </FormGroup>
+                                    </Col>
+                                    <Col sm={3}>
+                                        <FormGroup>
+                                            <Button id="signInPageButtons" value="approved" onClick={() => this.goToSignUpPage()}>Create user</Button>
+                                        </FormGroup>
+                                    </Col>
+                                    <Col sm={3}/>
+                                </Row>
+                            </Form>
+                        </React.Fragment>
+
                     </div>
                     <img src={image} alt="icon" id="icon"/>
                 </div>
-          </div>
+            </div>
 
-      );
+        );
     }
-  }
+}
 
